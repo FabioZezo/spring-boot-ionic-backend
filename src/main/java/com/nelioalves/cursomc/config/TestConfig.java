c5453bee755b295801bb14082a58e1355e09235e
Fabio Kruschelski <bmx.fabio@gmail.com>
2023-02-22 16:16:04 -0300

    MockEmailService com Logger. Padrões Strategy e Template Method

--- src/main/java/com/nelioalves/cursomc/config/TestConfig.java
+++ src/main/java/com/nelioalves/cursomc/config/TestConfig.java
@@ -1,6 +1,8 @@
 package com.nelioalves.cursomc.config;
 
 import com.nelioalves.cursomc.services.DBService;
+import com.nelioalves.cursomc.services.EmailService;
+import com.nelioalves.cursomc.services.MockEmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,10 +19,12 @@ public class TestConfig {
 
     @Bean
     public boolean instantiateDatabase() throws ParseException {
-
         dbService.instantiateTestDatabase();
-
         return true;
+    }
 
+    @Bean
+    public EmailService emailService(){
+        return new MockEmailService();
     }
 }
